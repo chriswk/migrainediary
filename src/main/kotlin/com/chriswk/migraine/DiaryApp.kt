8325dc877db5eb3654ca3b3f673a916e3b293845
Christopher Kolstad <christopher.kolstad@finn.no>
2018-03-15 16:11:52 +0100

    Added prometheus metrics

--- src/main/kotlin/com/chriswk/migraine/DiaryApp.kt
+++ src/main/kotlin/com/chriswk/migraine/DiaryApp.kt
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-open class DiaryApp {
+class DiaryApp {
+
+
 }
 
 fun main(args: Array<String>) {
